9ba252adbca8482f7f13c3ab2a4589d581a18701
Martijn van Exel <m@rtijn.org>
2015-12-05 14:49:29 -0700

    templating

--- maproulette/views.py
+++ maproulette/views.py
@@ -1,20 +1,20 @@
-from django.http import HttpResponse
-from django.template import RequestContext, loader
+from django.shortcuts import render
+from django.http import HttpResponse, Http404
 
-from .models import Challenge
+from .models import Challenge, Task
 
 def index(request):
 	challenge_list = Challenge.objects.order_by('name')[:5]
-	template = loader.get_template('maproulette/index.html')
-	context = RequestContext(request, {
-        'challenge_list': challenge_list,
-    })
-	return HttpResponse(template.render(context))
+	context = {'challenge_list': challenge_list}
+	return render(request, 'maproulette/index.html', context)
 
 
 def task(request, task_identifier):
-	return HttpResponse("This is task {}".format(task_identifier))
-
+    try:
+        task = Task.objects.get(pk=task_identifier)
+    except Task.DoesNotExist:
+        raise Http404("Task does not exist")
+    return render(request, 'maproulette/task.html', {'task_identifier': task_identifier})
 
 def challenge(request, challenge_slug):
 	return HttpResponse("This is challenge {}".format(challenge_slug))
