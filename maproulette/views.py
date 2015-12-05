0bafb5b600a88c34858097fa238213f3aff0de21
Martijn van Exel <m@rtijn.org>
2015-12-05 15:30:53 -0700

    smarter challenge view, add 404 shortcut

--- maproulette/views.py
+++ maproulette/views.py
@@ -1,38 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
 
 from .models import Challenge, Task
 
 def index(request):
-	challenge_list = Challenge.objects.order_by('name')[:5]
-	context = {'challenge_list': challenge_list}
-	return render(request, 'maproulette/index.html', context)
+    challenge_list = Challenge.objects.order_by('name')[:5]
+    context = {'challenge_list': challenge_list}
+    return render(request, 'maproulette/index.html', context)
 
 
 def task(request, challenge_slug, task_identifier):
-    try:
-        task = Task.objects.get(
-        	challenge=challenge_slug,
-        	pk=task_identifier)
-    except Task.DoesNotExist:
-        raise Http404("Task does not exist")
+    task = get_object_or_404(
+    	Task,
+        challenge=challenge_slug,
+        pk=task_identifier)
     return render(
-    	request,
-    	'maproulette/task.html',
-    	{
-    		'challenge_slug': challenge_slug,
-    		'task_identifier': task_identifier
-    	})
+        request,
+        'maproulette/task.html',
+        {
+            'task': task,
+        })
 
 def challenge(request, challenge_slug):
-    try:
-        task = Challenge.objects.get(
-        	pk=challenge_slug)
-    except Challenge.DoesNotExist:
-        raise Http404("Challenge does not exist")
+    challenge = get_object_or_404(
+        Challenge,
+        pk=challenge_slug)
     return render(
-    	request,
-    	'maproulette/challenge.html',
-    	{
-    		'challenge_slug': challenge_slug,
-    	})
+        request,
+        'maproulette/challenge.html',
+        {
+            'challenge': challenge,
+        })
