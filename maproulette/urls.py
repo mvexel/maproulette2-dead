42dd7dcc39345e2cbba82c1a647d4baab7b3bb02
Martijn van Exel <m@rtijn.org>
2015-12-05 15:59:40 -0700

    more linkification

--- maproulette/urls.py
+++ maproulette/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'maproulette'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^challenge/(?P<challenge_slug>[0-9a-zA-Z\-]+)/task/(?P<task_identifier>[0-9a-zA-Z\-]+)/$', views.task, name='task'),
