bf31370d8050f2d754c2cd761efac4bc8689e91d
Martijn van Exel <m@rtijn.org>
2016-02-24 16:06:02 -0800

    clean master

--- maproulette/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-from .models import Task, Challenge
-
-admin.site.register([Task, Challenge])
--- maproulette/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MaprouletteConfig(AppConfig):
-    name = 'maproulette'
